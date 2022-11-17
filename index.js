684141929537d25fdfe155f2a51b073762de6052
erickexinlu <erickexinlu@gmail.com>
2022-11-16 22:07:18 -0800

    Implement redirect for authorization

--- index.js
+++ index.js
@@ -1,22 +1,86 @@
+require('dotenv').config()
 const express = require('express');
-
 const app = express();
+const querystring = require('querystring');
+const axios = require('axios');
+const port = 8888;
+
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
+
+console.log(CLIENT_ID);
 
 app.get('/', (req, res) => {
     const data = {
         name: 'Eric',
-        isUnemployed: true
+        isAwesome: true
     };
 
     res.json(data);
 });
 
-app.get('/awesome-generator', (req, res) => {
-    const { name, isAwesome } = req.query;
-    res.send(`${name} is ${JSON.parse(isAwesome) ? 'really' : 'not'} awesome`);
-});
+/**
+ * Generates a random string containing numbers and letters
+ * @param  {number} length The length of the string
+ * @return {string} The generated string
+ */
+const generateRandomString = length => {
+    let text = '';
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (let i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+};
 
-const port = 8888;
+const stateKey = 'spotify_auth_state';
+
+app.get('/login', (req, res) => {
+    const state = generateRandomString(16);
+    res.cookie(stateKey, state);
+
+    const scope = 'user-read-private user-read-email';
+
+    const queryParams = querystring.stringify({
+        client_id: CLIENT_ID,
+        response_type: 'code',
+        redirect_uri: REDIRECT_URI,
+        state: state,
+        scope: scope
+    })
+    res.redirect(`https://accounts.spotify.com/authorize?${queryParams}`);
+})
+
+app.get('/callback', (req, res) => {
+    const code = req.query.code || null;
+
+    axios({
+        method: 'post',
+        url: 'https://accounts.spotify.com/api/token',
+        data: querystring.stringify({
+            grant_type: 'authorization_code',
+            code: code,
+            redirect_uri: REDIRECT_URI
+        }),
+        headers: {
+            'content-type': 'application/x-www-form-urlencoded',
+            Authorization: `Basic ${new Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64')}`,
+        },
+    })
+        .then(response => {
+            if (response.status === 200) {
+                res.send(`<pre>${JSON.stringify(response.data, null, 2)}</pre>`);
+            } else {
+                res.send(response);
+            }
+        })
+        .catch(error => {
+            res.send(error);
+        });
+})
+
+
 app.listen(port, () => {
     console.log(`Express app listening at http://localhost:${port}`);
 });

12df8a2f5a966c61964abf9274f87c353536650f
erickexinlu <erickexinlu@gmail.com>
2022-11-22 01:44:07 -0800

    Implement top tracks

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,13 +1,14 @@
 import { useState, useEffect } from 'react';
 import { catchErrors } from '../utils';
-import { getCurrentUserProfile, getCurrentUserPlaylists, getTopArtists } from '../spotify';
-import { SectionWrapper, ArtistsGrid } from '../components';
+import { getCurrentUserProfile, getCurrentUserPlaylists, getTopArtists, getTopTracks } from '../spotify';
+import { SectionWrapper, ArtistsGrid, TrackList } from '../components';
 import { StyledHeader } from '../styles';
 
 const Profile = () => {
   const [profile, setProfile] = useState(null);
   const [playlists, setPlaylists] = useState(null);
   const [topArtists, setTopArtists] = useState(null);
+  const [topTracks, setTopTracks] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -20,7 +21,8 @@ const Profile = () => {
       const userTopArtists = await getTopArtists();
       setTopArtists(userTopArtists.data);
 
-      console.log(userTopArtists.data);
+      const userTopTracks = await getTopTracks();
+      setTopTracks(userTopTracks.data);
     };
 
     catchErrors(fetchData());
@@ -49,10 +51,14 @@ const Profile = () => {
               </div>
             </div>
           </StyledHeader>
-          {topArtists && (
+          {topArtists && topTracks && (
             <main>
               <SectionWrapper title="Top artists this month" seeAllLink="/top-artists">
                 <ArtistsGrid artists={topArtists.items.slice(0, 10)} />
+              </SectionWrapper>
+
+              <SectionWrapper title="Top tracks this month" seeAllLink="/top-tracks">
+                <TrackList tracks={topTracks.items.slice(0, 10)} />
               </SectionWrapper>
             </main>
           )}

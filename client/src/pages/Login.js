88a9b72a658ddb02bfd59a65fc81820e057d71e5
erickexinlu <erickexinlu@gmail.com>
2022-11-29 21:40:21 -0800

    Switch back to heroku

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -27,7 +27,7 @@ const StyledLoginButton = styled.a`
 const LOGIN_URI =
   process.env.NODE_ENV !== 'production'
     ? 'http://localhost:8888/login'
-    : 'https://spotify-api-plus.onrender.com/callback';
+    : 'https://spotify-api-plus.herokuapp.com/login';
 
 const Login = () => (
   <StyledLoginContainer>
